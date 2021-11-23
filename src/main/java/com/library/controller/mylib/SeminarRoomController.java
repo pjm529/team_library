74b5addc3c7f5cdbf05a13b54729b5c85e8fae02
hyeri01 <fia_re@naver.com>
2021-11-23 10:33:23 +0900

    캘린더 예약관리 click 시, seminarRoomService.jsp로 이동 Controller

---
+++ src/main/java/com/library/controller/mylib/SeminarRoomController.java
@@ -0,0 +1,21 @@
+package com.library.controller.mylib;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/mylib/*")
+public class SeminarRoomController {
+	
+	@GetMapping("/seminarRoomService")
+	public String seminarRoomService(@RequestParam("nowDate") String unowDate, Model model) {
+		
+		
+		
+		return "/mylib/sub3/seminarRoomService";
+	}
+
+}
