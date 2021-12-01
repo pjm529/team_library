f209002a4b1171dcec82cd8c0c4a8e7c1b241dec
hyeri01 <fia_re@naver.com>
2021-12-01 10:41:54 +0900

    주석 수정

--- src/main/java/com/library/model/mylib/NoteBookRoomDTO.java
+++ src/main/java/com/library/model/mylib/NoteBookRoomDTO.java
@@ -7,9 +7,9 @@ import lombok.Data;
 @Data
 public class NoteBookRoomDTO {
 
-	private int seat_no;
-	private String user_id;
-	private Timestamp checkin_time;
-	private Timestamp checkout_time;
-	private long diff_time;
+	private int seat_no; // 좌석 번호
+	private String user_id; // 예약자 id
+	private Timestamp checkin_time; // 입실 시간
+	private Timestamp checkout_time; // 퇴실 시간
+	private long diff_time; // 잔여 시간
 }
