60fb115707aa9426a1485ce8e6619ce90292a37c
hyeri01 <fia_re@naver.com>
2021-12-01 11:22:52 +0900

    전체 Reading Room 좌석 세팅

---
+++ src/test/java/com/library/roomTest/ReadingRoomTest.java
@@ -0,0 +1,29 @@
+package com.library.roomTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.library.service.mylib.NotebookRoomService;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReadingRoomTest {
+	
+	@Autowired
+	private NotebookRoomService service;
+	
+	@Test
+	public void roomInsert() {
+		
+		for(int i = 1; i < 125; i++) {
+			service.insert(i);
+		}
+	}
+
+}
