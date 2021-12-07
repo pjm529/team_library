007563e200386af579d64c40d25ada399d561ac9
pjm529 <pok_nima@naver.com>
2021-12-07 15:31:14 +0900

    home 에 접속하고 첨부파일 기본 디렉토리 없을 시 생성

--- src/main/java/com/library/controller/HomeController.java
+++ src/main/java/com/library/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.library.controller;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -20,50 +21,63 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		// 첨부파일 디렉토리
+		String path = "C:/library_file";
+		File folder = new File(path);
+
+		// 해당 디렉토리 확인
+		if (!folder.exists()) {
+			try {
+				folder.mkdirs(); // 폴더 생성
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		}
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
 	@GetMapping("/logout")
 	public String logout(HttpServletRequest request) {
-		
+
 		HttpSession session = request.getSession();
-		
+
 		// 세션 초기화
 		session.invalidate();
-		
+
 		return "redirect:/";
-		
+
 	}
-	
+
 	@GetMapping("/accessError")
 	public String accessDenied() {
 
 		return "/error/accessError";
-		
+
 	}
-	
+
 	@GetMapping("/accessError2")
 	public String accessDenied2() {
 
 		return "/error/accessError2";
-		
+
 	}
-	
+
 }
