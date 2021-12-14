3b9907a811f96b2e5b9456086188f32de51328e0
pjm529 <pok_nima@naver.com>
2021-12-14 12:33:26 +0900

    주석 추가

--- src/main/java/com/library/util/DateUtil.java
+++ src/main/java/com/library/util/DateUtil.java
@@ -9,14 +9,22 @@ public class DateUtil {
 		Date now = new Date();
 		Calendar cal = Calendar.getInstance();
 
+		// 현재 시간으로 설정
 		cal.setTime(now);
 
+		// 현재 날짜의 년
 		int year = cal.get(Calendar.YEAR);
+		
+		// 현재 날짜의 월
 		int month = cal.get(Calendar.MONTH) + 1;
 
 		if (type.equals("year")) {
+			
+			// year 리턴
 			return Integer.toString(year);
 		} else {
+			
+			// month 리턴
 			return Integer.toString(month);
 		}
 	}
